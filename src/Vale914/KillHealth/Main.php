1e10057e8a78af5aca7413b888eca9d19eef8adb
Johnsonk79 <45489804+Johnsonk79@users.noreply.github.com>
2020-01-29 11:36:08 +0800

    Update Main.php

--- src/Vale914/KillHealth/Main.php
+++ src/Vale914/KillHealth/Main.php
@@ -8,6 +8,8 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
 
 class Main extends PluginBase implements Listener{
 
@@ -25,7 +27,8 @@ class Main extends PluginBase implements Listener{
             $damager = $cause->getDamager();
             if($damager instanceof Player){
                 if(in_array($event->getPlayer()->getLevel()->getFolderName(), $this->getConfig()->get("worlds"))){
-                    $damager->setHealth($damager->getMaxHealth());
+                    $damager->addEffect(new EffectInstance(Effect::getEffect(Effect::REGENERATION), 2, 2, true));
+                    $damager->addEffect(new EffectInstance(Effect::getEffect(Effect::STRENGTH), 1, 1, true));
                     $damager->sendMessage(str_replace(["{player}", "{killername}"], [$event->getPlayer()->getName(), $damager->getName()], $this->getConfig()->get("killer-message")));
                     $event->setDeathMessage(str_replace(["{player}", "{killername}"], [$event->getPlayer()->getName(), $damager->getName()], $this->getConfig()->get("death-message")));
                 }
